738be5fd0394a83170124ef3f8a513a0358049e4
Sergey Plashenko <sergey.plashenko@sitecore.com>
2019-02-21 10:34:55 +0200

    HtmlMailFiller and FillEmail from Reflector.

---
+++ src/Sitecore.Support.312341/EmailCampaign/Cm/Pipelines/SendEmail/FillEmail.cs
@@ -0,0 +1,48 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.EmailCampaign.Cm.Pipelines.SendEmail;
+using Sitecore.ExM.Framework.Diagnostics;
+using Sitecore.Modules.EmailCampaign.Core.Crypto;
+using Sitecore.Modules.EmailCampaign.Messages;
+
+namespace Sitecore.Support.EmailCampaign.Cm.Pipelines.SendEmail
+{
+  public class FillEmail
+  {
+    private readonly IStringCipher _cipher;
+    private readonly ILogger _logger;
+
+    public FillEmail(IStringCipher cipher, ILogger logger)
+    {
+      Assert.ArgumentNotNull(cipher, "cipher");
+      Assert.ArgumentNotNull(logger, "logger");
+      _cipher = cipher;
+      _logger = logger;
+    }
+
+    private MailFiller GetMailFiller(SendMessageArgs args)
+    {
+      var ecmMessage = args.EcmMessage as HtmlMailBase;
+      if (ecmMessage != null)
+      {
+        return new HtmlMailFiller(ecmMessage, args, _logger, _cipher);
+      }
+
+      var message = args.EcmMessage as MailMessageItem;
+      if (message != null)
+      {
+        return new MailMessageFiller(message, args, _cipher);
+      }
+
+      return null;
+    }
+
+    public void Process(SendMessageArgs args)
+    {
+      if (args.EcmMessage != null)
+      {
+        var mailFiller = GetMailFiller(args);
+        mailFiller?.FillEmail();
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.312341/EmailCampaign/Cm/Pipelines/SendEmail/HtmlMailFiller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Sitecore.Diagnostics;
+using Sitecore.EDS.Core.Dispatch;
+using Sitecore.EmailCampaign.Cm.Pipelines.SendEmail;
+using Sitecore.ExM.Framework.Diagnostics;
+using Sitecore.Modules.EmailCampaign;
+using Sitecore.Modules.EmailCampaign.Core.Crypto;
+using Sitecore.Modules.EmailCampaign.Messages;
+
+namespace Sitecore.Support.EmailCampaign.Cm.Pipelines.SendEmail
+{
+  public class HtmlMailFiller : Sitecore.EmailCampaign.Cm.Pipelines.SendEmail.HtmlMailFiller
+  {
+    private readonly HtmlMailBase _htmlMailBase;
+
+    public HtmlMailFiller(HtmlMailBase message, SendMessageArgs args, ILogger logger, IStringCipher cipher) : base(
+      message, args, logger, cipher)
+    {
+      Assert.ArgumentNotNull(message, "message");
+      _htmlMailBase = message;
+    }
+
+    protected override void FillBody()
+    {
+      var utcNow = DateTime.UtcNow;
+      var body = _htmlMailBase.Body;
+      Email.ContentType = MessageContentType.Html;
+      Email.HtmlBody = _htmlMailBase.ReplaceTokens(body);
+      Util.TraceTimeDiff("SetEmailHtmlBody(ReplaceTokens(Body))", utcNow);
+      utcNow = DateTime.UtcNow;
+      Email.PlainTextBody = _htmlMailBase.ReplaceTokens(_htmlMailBase.AlternateText);
+      Util.TraceTimeDiff("SetEmailAltBody(ReplaceTokens(AlternateText))", utcNow);
+    }
+  }
+}
